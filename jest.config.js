8bf0ff6a49a6056e059285b010fadf5deb9fd31e
elbica <sh0705k@gmail.com>
2022-09-10 20:42:57 +0900

    test : jest, testing-library 초기 설정
    - jest.config.js 작성
    - 테스트 모듈 설치

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  moduleDirectories: ['node_modules'],
+  rootDir: './src',
+  moduleNameMapper: {
+    '^@application/(.*)$': '<rootDir>/application/$1',
+    '^@component/(.*)$': '<rootDir>/component/$1',
+    '^@domain/(.*)$': '<rootDir>/domain/$1',
+    '^@infra/(.*)$': '<rootDir>/infra/$1',
+    '^@page/(.*)$': '<rootDir>/page/$1',
+    '^@helper/(.*)$': '<rootDir>helpers/$1',
+  },
+
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
